69a8c85ca4a26c10ec66933f1502dad80b1c8890
jonh1036 <jonh1036@hotmail.com>
2018-07-27 15:01:48 -0300

    Criação de funções para o player 1
    Criada funções que movimentam para cima e para baixo a barra do player 1.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 char mat[7][7], opcao;
 char player1[15], player2[15];
 int vPlayer1 = 0, vPlayer2 = 0;
+int p1X = 0, p1Y = 3, p2A = 6, p2B = 3, bolaX = 3, bolaY = 3;
 
 int main(){
 	inicializaNomes();
--- mover.h
+++ mover.h
@@ -1,14 +1,29 @@
 #include<stdio.h>
 
 extern char mat[7][7], opcao;
+extern int p1X, p1Y, p2A, p2B, bolaX, bolaY;
+
+void verifica();
+void baixoPlayer1();
+void cimaPlayer1();
 
 void verifica(){
 	switch(opcao){
 		case 113: exit(0);//Encerrar o jogo
 			break;
-		case 115: //baixo();//Movimentar para baixo a barra do player 1
+		case 115: baixoPlayer1();//Movimentar para baixo a barra do player 1
 			break;
-		case 119: //cima();//Movimentar para cima a barra do player 1
+		case 119: cimaPlayer1();//Movimentar para cima a barra do player 1
 			break;
 	}
+}
+
+void baixoPlayer1(){
+	if(p1Y<6)
+		p1Y++;
+}
+
+void cimaPlayer1(){
+	if(p1X>0)
+		p1X++;
 }
